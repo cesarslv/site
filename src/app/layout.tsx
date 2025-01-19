ba0f9eb24bc9ae42d0f253713ddf3500f71b5a8d
Cesar <ccesarslv@gmail.com>
2025-01-19 18:18:45 -0300

    feat: add metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,4 @@
-import { cn } from "@/lib/utils"
+import { absoluteUrl, cn } from "@/lib/utils"
 import "./globals.css"
 
 import type { Metadata } from "next"
@@ -15,8 +15,39 @@ const geistMono = Geist_Mono({
 })
 
 export const metadata: Metadata = {
-  title: "cesar",
-  description: "software developer",
+  title: {
+    default: "cesar",
+    template: "%s - Personal website of cesar, built with Next.js.",
+  },
+
+  description: "Personal website of cesar, built with Next.js.",
+  keywords: ["nextjs", "react", "react server components", "cesar"],
+  authors: [
+    {
+      name: "cesar",
+      url: "https://cesarslv.com",
+    },
+  ],
+  creator: "cesar",
+  openGraph: {
+    type: "website",
+    locale: "en_US",
+    url: "https://cesarslv.com",
+    title: "cesar",
+    description: "Personal website of cesar, built with Next.js.",
+    siteName: "cesar",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "cesar",
+    description: "Personal website of cesar, built with Next.js.",
+    images: ["https://cesarslv.com/opengraph-image.png"],
+    creator: "@sadmann17",
+  },
+  icons: {
+    icon: "/icon.png",
+  },
+  manifest: absoluteUrl("/site.webmanifest"),
 }
 
 export default function RootLayout({
